86167bcb2ff8e0feb4d98b5afb835c733dec0e11
Sourav Routh <67389838+SouravR7@users.noreply.github.com>
2020-11-05 20:38:52 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <>
+      <div id="root">
+        <p> I am learning React. My life is getting better.</p>
+      </div>
+    </>
+  );
+};
 
 export default App;
